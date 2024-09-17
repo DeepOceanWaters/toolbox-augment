1d540a477afba9c35958d0941e64b426d01d275e
Colin Bradford <cjthehippo@gmail.com>
2024-09-17 14:35:15 -0700

    updated content script

--- content-script.ts
+++ content-script.ts
@@ -1,3 +1,8 @@
-import main from "./content-script-main";
-
 main();
+async function main(): Promise<void> {
+    const { default: contentScript } = await import(
+        chrome.runtime.getURL("./content-script-main.js")
+    );
+    contentScript();
+}
+
